15130a3de6f2b0019c67067d2803093cdad74ec7
Mesele Assefa <getmesy@gmail.com>
2023-07-09 15:56:42 +0000

    correct pycodestyle

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -9,4 +9,8 @@ Input = List[float]
 
 
 def sum_list(input_list: Input) -> float:
+    """
+    a type-annotated function sum_list which takes a list input_list of
+    floats as argument and returns their sum as a float.
+    """
     return sum(input_list)
