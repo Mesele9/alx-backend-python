e2c6ef7837828be85222fdb291a46c478299611d
Mesele Assefa <getmesy@gmail.com>
2023-07-10 16:28:03 +0000

    change random.random with random.uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,6 +11,6 @@ async def wait_random(max_delay=10):
     waits for a random delay between 0 and max_delay (included and
     float value) seconds and eventually returns it.
     """
-    num = random.random() * max_delay
+    num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
