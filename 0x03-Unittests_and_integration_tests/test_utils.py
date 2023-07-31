5919f222e2f0e349f2272dc7a30cd538e6ea3a70
Mesele Assefa <getmesy@gmail.com>
2023-07-31 16:19:47 +0000

    push task1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """ a method to test access_nested_map function """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, result):
+        """ A method to implement keyError """
+        with self.assertRaises(KeyError) as err:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(err.exception), f"'{result}'")
