56e1fc3ad9de7da5f9bc01cf782fee95fa9c136e
Mesele Assefa <getmesy@gmail.com>
2023-07-31 15:13:12 +0000

    documentation

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,4 +14,5 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
+        """ a method to test access_nested_map function """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
