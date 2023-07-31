3f28eeb0670ad3aa62ac9dda39d48fae96307160
Mesele Assefa <getmesy@gmail.com>
2023-07-31 21:26:16 +0000

    push  task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,3 +23,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_json.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(orgName)
         )
+
+    def test_public_repos_url(self) -> None:
+        """ method to test public_repos_url property """
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mocked_org:
+            mocked_org.return_value = {
+                    'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                    GithubOrgClient("google")._public_repos_url,
+                    "https://api.github.com/users/google/repos",
+            )
