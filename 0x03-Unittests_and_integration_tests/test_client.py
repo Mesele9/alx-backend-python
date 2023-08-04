cf44a9ac0af90dc7228222a3381decf1288edd28
Mesele Assefa <getmesy@gmail.com>
2023-08-04 16:55:17 +0000

    corregion

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,13 +3,14 @@
 from typing import Dict
 import unittest
 from unittest.mock import patch, Mock, MagicMock, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parametirized_class
 from client import GithubOrgClient
+from requests import HTTPError
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """ class to test GithubOrgCleint fucntion """
-
     @parameterized.expand([
         ("google", {'login': "google"}),
         ("abc", {'login': "abc"}),
@@ -37,3 +38,26 @@ class TestGithubOrgClient(unittest.TestCase):
                     GithubOrgClient("google")._public_repos_url,
                     "https://api.github.com/users/google/repos",
             )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json: MagicMock) -> None:
+        """ Test publi_repos method"""
+        repos_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+
+        with patch.object(GithubOrgClient,
+                         '_public_repos_url',
+                         new_callable=PropertyMock
+                         ) as mock_public_repos_url:
+            mock_json.return_value = repos_payload
+            org_client = GithubOrgClient("google")
+            repos_list = org_client.public_repos()
+
+            mock_json.assert_called_once_with(
+                "https://api.github.com/users/google/repos"
+            )
+
+            self.assertEqual(repos_list, repos_payload)
